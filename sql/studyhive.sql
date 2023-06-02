9fea234c1d08c645a96f37d4310640285ff58976
Painapple <105093357+Pain4pple@users.noreply.github.com>
2023-06-02 19:52:10 +0800

    Update studyhive.sql

--- sql/studyhive.sql
+++ sql/studyhive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2023 at 04:15 PM
+-- Generation Time: Jun 02, 2023 at 01:51 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -51,7 +51,8 @@ INSERT INTO `comments` (`CommentID`, `PostID`, `UserID`, `Body`, `Date`, `Upvote
 (21, 1, 7, '<p><img src=\"https://media.discordapp.net/attachments/1066306547644371048/1109449749922328607/2aec23ca013160eb.gif\"></p>', '2023-05-21 11:26:43', 0),
 (22, 3, 9, '<p>test</p>', '2023-05-27 23:48:05', 0),
 (23, 3, 6, '<p>I can comment because I am logged in!!</p>', '2023-06-01 10:58:01', 0),
-(24, 9, 4, '<p>cute~</p>', '2023-06-01 21:41:35', 0);
+(24, 9, 4, '<p>cute~</p>', '2023-06-01 21:41:35', 0),
+(35, 15, 5, '<p>oks lang haha</p>', '2023-06-02 18:05:34', 0);
 
 -- --------------------------------------------------------
 
@@ -102,8 +103,6 @@ CREATE TABLE `nestedreplies` (
 INSERT INTO `nestedreplies` (`NestedReplyID`, `ParentReplyID`, `UserID`, `Body`, `Date`, `UpvoteCount`, `replyingTo`) VALUES
 (1, 1, 7, 'ㅋㅋㅋㅋ', '2023-05-19 00:00:00', 231, 4),
 (2, 1, 8, '<p>wazzaproblemm8?</p>', '2023-05-20 09:19:50', 0, 4),
-(3, 25, 8, '<p>aww doin be mad</p>', '2023-05-20 09:31:09', 0, 5),
-(4, 25, 8, '<p>u make me sad </p>', '2023-05-20 09:31:42', 0, 5),
 (5, 1, 8, '<p>?????</p>', '2023-05-20 09:38:04', 0, 4),
 (6, 1, 7, '<p>grrrrrrrr</p>', '2023-05-21 11:51:13', 0, 7);
 
@@ -130,10 +129,10 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`PostID`, `UserID`, `CommunityID`, `Title`, `Body`, `Media`, `Date`, `UpvoteCount`, `Comments`) VALUES
-(1, 5, 1, '[안내📢]<br/>\r\nNewJeans 1st Fan Meeting \'Bunnies Camp\'', '<p>📅2023.5.15. MON. 2PM (KST) ~ 2023.5.17. WED. 2PM (KST)</p>\r\n<p>🔗<a href=\"https://weverse.io/newjeansofficial/notice/13470\">https://weverse.io/newjeansofficial/notice/13470</a></p>\r\n\r\n<p>#NewJeans #뉴진스</p>\r\n<p>#Bunnies_Camp #버니즈캠프</p>', '/resources/images/postMedias/postMedia_347256548_101422482963339_9191258991744063554_n.jpg', '2023-05-15 00:00:00', 681, 6),
-(3, 6, 1, 'Happy Birthday, Haerin! 🐱🍰', '', '/resources/images/postMedias/postMedia_Fv-myDmWAAAuprR.jpg', '2023-05-15 14:37:16', 2397, 18),
-(8, 6, 1, 'Hello, Tokkis~', '<p>This is a test post</p>', '', '2023-06-01 20:58:30', 1, 0),
-(9, 6, 2, 'rawrrr', '<p><img src=\"https://i.pinimg.com/564x/4b/3b/14/4b3b14ea19f48647bf3e0ef11de36ded.jpg\"></p>', '', '2023-06-01 21:09:06', 3, 2);
+(1, 5, 1, '[안내📢]<br/>NewJeans 1st Fan Meeting \'Bunnies Camp\'', '<p>📅2023.5.15. MON. 2PM (KST) ~ 2023.5.17. WED. 2PM (KST)</p><p>🔗<a href=\"https://weverse.io/newjeansofficial/notice/13470\" target=\"_blank\">https://weverse.io/newjeansofficial/notice/13470</a></p><p>#NewJeans #뉴진스</p><p>#Bunnies_Camp #버니즈캠프</p>', '/resources/images/postMedias/postMedia_347256548_101422482963339_9191258991744063554_n.jpg', '2023-05-15 00:00:00', 683, 5),
+(3, 6, 1, 'Happy Birthday, Haerin! 🐱🍰', '', '/resources/images/postMedias/postMedia_Fv-myDmWAAAuprR.jpg', '2023-05-15 14:37:16', 2399, 17),
+(9, 6, 2, 'rawrrrr!!', '<p><img src=\"https://i.pinimg.com/564x/4b/3b/14/4b3b14ea19f48647bf3e0ef11de36ded.jpg\"></p>', '', '2023-06-01 21:09:06', 5, 2),
+(15, 9, 2, 'Thoughts about IT Automation?', '<p>Hi! I am an incoming third year student in IT. I am still undecided on what track I should pursue. I feel pretty safe with web development but I really want to try and I am really curious about IT automation. Are there any automation students here who can share their experiences on the strand? How is it so far and is it worth it? Thank you!</p>', '', '2023-06-02 11:30:15', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -164,7 +163,8 @@ INSERT INTO `replies` (`ReplyID`, `ParentCommentID`, `UserID`, `Body`, `Date`, `
 (32, 10, 9, '<p>😻🤍</p>', '2023-05-20 08:32:51', 0, 'testComment'),
 (33, 16, 7, '<p>let\'s gaur</p>', '2023-05-21 11:22:33', 0, ''),
 (34, 15, 7, '<p><img src=\"https://media.discordapp.net/attachments/1066306547644371048/1109446698092216412/4a5c0836a4fe2b38.gif\"></p>', '2023-05-21 11:24:04', 0, ''),
-(35, 24, 7, '<p>you never call me cute :((</p>', '2023-06-01 21:48:42', 0, '');
+(35, 24, 7, '<p>you never call me cute :((</p>', '2023-06-01 21:48:42', 0, ''),
+(41, 35, 9, '<p>thanks...</p>', '2023-06-02 19:09:57', 0, '');
 
 -- --------------------------------------------------------
 
@@ -192,20 +192,21 @@ CREATE TABLE `user` (
   `Username` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `ProfileImage` text NOT NULL,
-  `DateJoined` date DEFAULT NULL
+  `DateJoined` date DEFAULT NULL,
+  `Bio` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `Email`, `Username`, `Password`, `ProfileImage`, `DateJoined`) VALUES
-(4, 'hanni.pham@adorlabels.kr', 'hanni_bunch', '5af83e3196bf99f440f31f2e1a6c9afe', '/resources/images/userImages/UserImage_6461a01d7c7ee6.21941254.jpg', '2023-05-03'),
-(5, 'kang_hwaewin@adorlabels.kr', 'haerin~', '1955598fff3c9d982b8fe344d59fe955', '/resources/images/userImages/UserImage_6461c9ac651d75.39312207.jpg', '2023-04-28'),
-(6, 'lee.hyein@adorlabels.kr', 'hyeinnn', '4c4a9fd7f4a4a416922b64bb3060bcd3', '/resources/images/userImages/UserImage_6461cb26174329.13978396.jpg', '2023-05-01'),
-(7, 'kim.min-ji@adorlabels.kr', 'minji.tokki', '1ff61e91349d3f6623a81ccd3d881fa1', '/resources/images/userImages/UserImage_6462c6c1ab4b78.35138837.png', '2023-04-25'),
-(8, 'daniellejune.marsh@adorlabels.kr', 'sunflower-dani', '41fca3a29aaddd9a004fd0156a45426b', '/resources/images/userImages/UserImage_64669f59eac3c8.57397346.jpg', '2023-05-03'),
-(9, 'kittenstuart@gmail.com', 'stuart', '407cb3436f86acc235172e57027af714', '/resources/images/userImages/UserImage_6466a09c92c8b5.46430806.jpg', '2023-05-12');
+INSERT INTO `user` (`UserID`, `Email`, `Username`, `Password`, `ProfileImage`, `DateJoined`, `Bio`) VALUES
+(4, 'hanni.pham@adorlabels.kr', 'hanni_bunch', '5af83e3196bf99f440f31f2e1a6c9afe', '/resources/images/userImages/UserImage_6461a01d7c7ee6.21941254.jpg', '2023-05-03', ''),
+(5, 'kang_hwaewin@adorlabels.kr', 'haerin~', '1955598fff3c9d982b8fe344d59fe955', '/resources/images/userImages/UserImage_6461c9ac651d75.39312207.jpg', '2023-04-28', ''),
+(6, 'lee.hyein@adorlabels.kr', 'hyeinnn', '4c4a9fd7f4a4a416922b64bb3060bcd3', '/resources/images/userImages/UserImage_6461cb26174329.13978396.jpg', '2023-05-01', ''),
+(7, 'kim.min-ji@adorlabels.kr', 'minji.tokki', '1ff61e91349d3f6623a81ccd3d881fa1', '/resources/images/userImages/UserImage_6462c6c1ab4b78.35138837.png', '2023-04-25', ''),
+(8, 'daniellejune.marsh@adorlabels.kr', 'sunflower-dani', '41fca3a29aaddd9a004fd0156a45426b', '/resources/images/userImages/UserImage_64669f59eac3c8.57397346.jpg', '2023-05-03', ''),
+(9, 'kittenstuart@gmail.com', 'stuart', '407cb3436f86acc235172e57027af714', '/resources/images/userImages/UserImage_6466a09c92c8b5.46430806.jpg', '2023-05-12', '<p>I am stuart</p>');
 
 -- --------------------------------------------------------
 
@@ -224,20 +225,24 @@ CREATE TABLE `uservotes` (
 --
 
 INSERT INTO `uservotes` (`PostID`, `UserID`, `Vote`) VALUES
-(1, 5, 0),
-(3, 5, 0),
+(1, 5, 1),
+(3, 5, 1),
 (1, 6, 1),
 (3, 6, 1),
-(8, 6, 0),
 (9, 6, 0),
 (9, 4, 1),
-(8, 4, 1),
 (1, 4, 1),
 (3, 4, 1),
 (9, 7, 1),
 (9, 8, 1),
 (3, 8, 1),
-(1, 8, 1);
+(1, 8, 1),
+(3, 9, 1),
+(9, 9, 1),
+(1, 9, 1),
+(15, 6, 1),
+(15, 5, 1),
+(9, 5, 1);
 
 --
 -- Indexes for dumped tables
@@ -315,7 +320,7 @@ ALTER TABLE `uservotes`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `CommentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `CommentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `community`
@@ -333,13 +338,13 @@ ALTER TABLE `nestedreplies`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `replies`
 --
 ALTER TABLE `replies`
-  MODIFY `ReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `ReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `threads`
@@ -361,16 +366,16 @@ ALTER TABLE `user`
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`PostID`) REFERENCES `posts` (`PostID`);
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`PostID`) REFERENCES `posts` (`PostID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `nestedreplies`
 --
 ALTER TABLE `nestedreplies`
-  ADD CONSTRAINT `nestedreplies_ibfk_1` FOREIGN KEY (`ParentReplyID`) REFERENCES `replies` (`ReplyID`),
-  ADD CONSTRAINT `nestedreplies_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
-  ADD CONSTRAINT `nestedreplies_ibfk_3` FOREIGN KEY (`replyingTo`) REFERENCES `replies` (`UserID`);
+  ADD CONSTRAINT `nestedreplies_ibfk_1` FOREIGN KEY (`ParentReplyID`) REFERENCES `replies` (`ReplyID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `nestedreplies_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `nestedreplies_ibfk_3` FOREIGN KEY (`replyingTo`) REFERENCES `replies` (`UserID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `posts`
@@ -383,8 +388,8 @@ ALTER TABLE `posts`
 -- Constraints for table `replies`
 --
 ALTER TABLE `replies`
-  ADD CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
-  ADD CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`ParentCommentID`) REFERENCES `comments` (`CommentID`);
+  ADD CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`ParentCommentID`) REFERENCES `comments` (`CommentID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `threads`
