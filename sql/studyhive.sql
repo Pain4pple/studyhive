74dc597df245fb04d2249de5e27ce23fe37e7534
Painapple <105093357+Pain4pple@users.noreply.github.com>
2023-06-03 11:05:21 +0800

    Demo

--- sql/studyhive.sql
+++ sql/studyhive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2023 at 01:51 PM
+-- Generation Time: Jun 03, 2023 at 05:04 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -78,7 +78,7 @@ CREATE TABLE `community` (
 
 INSERT INTO `community` (`CommunityID`, `Name`, `ShortName`, `Logo`, `Header`, `Description`, `Country`, `Rules`, `Date`) VALUES
 (1, 'Bunnies Camp', 'Tokkis', '/resources/images/community image.jpg', '/resources/images/header.jpg', 'An unofficial place for tokkis to gather and socialize with fellow bunnies. Dedicated to NewJeans.', 'Korea', '{\r\n  \"1\": \"Always apply basic etiquette.\",\r\n  \"2\": \"Submissions must be about NewJeans.\",\r\n  \"3\": \"Respect your fellow Tokkis.\",\r\n  \"4\": \"Limit NSFW posts, please.\",\r\n  \"5\": \"Use downvotes wisely!\",\r\n  \"6\": \"Spread the word!\"\r\n}', '2023-04-12'),
-(2, 'Tigers\' Lair', 'Thomasino', '/resources/images/schoolLogos/ust.png', '/resources/images/headerImages/ust-banner.jpg', 'A place for Thomasian cubs to gather and rawr 2gether', 'Philippines', '{\r\n  \"1\": \"Always apply basic etiquette.\",\r\n  \"2\": \"Submissions must be about UST.\",\r\n  \"3\": \"Respect your fellow Thomasians.\",\r\n  \"4\": \"Limit NSFW posts, please.\",\r\n  \"5\": \"Use downvotes wisely!\",\r\n  \"6\": \"No asking for School Recommendations\",\r\n  \"7\": \"Spread the word!\"\r\n}', '2023-06-01');
+(2, 'University of Santo Tomas', 'Thomasino', '/resources/images/schoolLogos/ust.png', '/resources/images/headerImages/ust-banner.jpg', 'A place for Tomasian cubs to gather and rawr 2gether', 'Philippines', '{\r\n  \"1\": \"Always apply basic etiquette.\",\r\n  \"2\": \"Submissions must be about UST.\",\r\n  \"3\": \"Respect your fellow Thomasians.\",\r\n  \"4\": \"Limit NSFW posts, please.\",\r\n  \"5\": \"Use downvotes wisely!\",\r\n  \"6\": \"No asking for School Recommendations\",\r\n  \"7\": \"Spread the word!\"\r\n}', '2023-06-01');
 
 -- --------------------------------------------------------
 
@@ -129,7 +129,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`PostID`, `UserID`, `CommunityID`, `Title`, `Body`, `Media`, `Date`, `UpvoteCount`, `Comments`) VALUES
-(1, 5, 1, '[안내📢]<br/>NewJeans 1st Fan Meeting \'Bunnies Camp\'', '<p>📅2023.5.15. MON. 2PM (KST) ~ 2023.5.17. WED. 2PM (KST)</p><p>🔗<a href=\"https://weverse.io/newjeansofficial/notice/13470\" target=\"_blank\">https://weverse.io/newjeansofficial/notice/13470</a></p><p>#NewJeans #뉴진스</p><p>#Bunnies_Camp #버니즈캠프</p>', '/resources/images/postMedias/postMedia_347256548_101422482963339_9191258991744063554_n.jpg', '2023-05-15 00:00:00', 683, 5),
+(1, 5, 1, '[안내📢]<br/>NewJeans 1st Fan Meeting \'Bunnies Camp\'', '<p>📅2023.5.15. MON. 2PM (KST) ~ 2023.5.17. WED. 2PM (KST)</p><p>🔗<a href=\"https://weverse.io/newjeansofficial/notice/13470\" target=\"_blank\">https://weverse.io/newjeansofficial/notice/13470</a></p><p>#NewJeans #뉴진스</p><p>#Bunnies_Camp #버니즈캠프</p>', '/resources/images/postMedias/postMedia_347256548_101422482963339_9191258991744063554_n.jpg', '2023-05-15 00:00:00', 684, 5),
 (3, 6, 1, 'Happy Birthday, Haerin! 🐱🍰', '', '/resources/images/postMedias/postMedia_Fv-myDmWAAAuprR.jpg', '2023-05-15 14:37:16', 2399, 17),
 (9, 6, 2, 'rawrrrr!!', '<p><img src=\"https://i.pinimg.com/564x/4b/3b/14/4b3b14ea19f48647bf3e0ef11de36ded.jpg\"></p>', '', '2023-06-01 21:09:06', 5, 2),
 (15, 9, 2, 'Thoughts about IT Automation?', '<p>Hi! I am an incoming third year student in IT. I am still undecided on what track I should pursue. I feel pretty safe with web development but I really want to try and I am really curious about IT automation. Are there any automation students here who can share their experiences on the strand? How is it so far and is it worth it? Thank you!</p>', '', '2023-06-02 11:30:15', 2, 2);
@@ -203,10 +203,12 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`UserID`, `Email`, `Username`, `Password`, `ProfileImage`, `DateJoined`, `Bio`) VALUES
 (4, 'hanni.pham@adorlabels.kr', 'hanni_bunch', '5af83e3196bf99f440f31f2e1a6c9afe', '/resources/images/userImages/UserImage_6461a01d7c7ee6.21941254.jpg', '2023-05-03', ''),
 (5, 'kang_hwaewin@adorlabels.kr', 'haerin~', '1955598fff3c9d982b8fe344d59fe955', '/resources/images/userImages/UserImage_6461c9ac651d75.39312207.jpg', '2023-04-28', ''),
-(6, 'lee.hyein@adorlabels.kr', 'hyeinnn', '4c4a9fd7f4a4a416922b64bb3060bcd3', '/resources/images/userImages/UserImage_6461cb26174329.13978396.jpg', '2023-05-01', ''),
+(6, 'lee.hyein@adorlabels.kr', 'hyeinnn', '4c4a9fd7f4a4a416922b64bb3060bcd3', '/resources/images/userImages/UserImage_6461cb26174329.13978396.jpg', '2023-05-01', '<p><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/NewJeans_Logo_1.svg/2560px-NewJeans_Logo_1.svg.png\" alt=\"File:NewJeans Logo 1.svg - Wikimedia Commons\"></p>'),
 (7, 'kim.min-ji@adorlabels.kr', 'minji.tokki', '1ff61e91349d3f6623a81ccd3d881fa1', '/resources/images/userImages/UserImage_6462c6c1ab4b78.35138837.png', '2023-04-25', ''),
 (8, 'daniellejune.marsh@adorlabels.kr', 'sunflower-dani', '41fca3a29aaddd9a004fd0156a45426b', '/resources/images/userImages/UserImage_64669f59eac3c8.57397346.jpg', '2023-05-03', ''),
-(9, 'kittenstuart@gmail.com', 'stuart', '407cb3436f86acc235172e57027af714', '/resources/images/userImages/UserImage_6466a09c92c8b5.46430806.jpg', '2023-05-12', '<p>I am stuart</p>');
+(9, 'kittenstuart@gmail.com', 'stuart', '407cb3436f86acc235172e57027af714', '/resources/images/userImages/UserImage_6466a09c92c8b5.46430806.jpg', '2023-05-12', '<p>I am stuart</p><p><br></p>'),
+(10, 'programmurLara@gmail.com', 'programmur', '202cb962ac59075b964b07152d234b70', '/resources/images/userImages/UserImage_6479f3e3396dd5.14397351.png', NULL, ''),
+(11, 'acert123@gmail.com', 'ace', 'aa5be6789de6fd2c9d7f53f79415e693', '/resources/images/userImages/UserImage_647aa96b475555.60583308.jpg', NULL, '<p>is my hair too short?</p>');
 
 -- --------------------------------------------------------
 
@@ -242,7 +244,11 @@ INSERT INTO `uservotes` (`PostID`, `UserID`, `Vote`) VALUES
 (1, 9, 1),
 (15, 6, 1),
 (15, 5, 1),
-(9, 5, 1);
+(9, 5, 1),
+(15, 9, 0),
+(1, 11, 1),
+(15, 11, 0),
+(9, 11, 0);
 
 --
 -- Indexes for dumped tables
@@ -320,7 +326,7 @@ ALTER TABLE `uservotes`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `CommentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `CommentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `community`
@@ -332,19 +338,19 @@ ALTER TABLE `community`
 -- AUTO_INCREMENT for table `nestedreplies`
 --
 ALTER TABLE `nestedreplies`
-  MODIFY `NestedReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `NestedReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `replies`
 --
 ALTER TABLE `replies`
-  MODIFY `ReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `ReplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `threads`
@@ -356,7 +362,7 @@ ALTER TABLE `threads`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
